74d2613379a66b45e8ac451d4e34121c39e94cc7
Deepak Apte <dapte@opentext.com>
2020-09-20 22:02:05 +0530

    finished auth flow

--- config/keys.js
+++ config/keys.js
@@ -1,7 +1,7 @@
 //keys.js - figure out what set od credential to return
 if (process.env.NODE_ENV === "production") {
   // we are in production
-  module.exports = git require("./prod");
+  module.exports = require("./prod");
 } else {
   // we are in development - i.e local machine
   module.exports = require("./dev");
