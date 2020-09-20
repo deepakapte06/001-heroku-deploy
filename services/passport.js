d98d69baa1df23cf5cc795be7c40776bb422e007
Deepak Apte <dapte@opentext.com>
2020-09-20 22:17:32 +0530

    finished auth flow1

--- services/passport.js
+++ services/passport.js
@@ -26,6 +26,7 @@ passport.use(
       clientID: keys.googleClientID,
       clientSecret: keys.googleClientSecret,
       callbackURL: "/auth/google/callback",
+      proxy: true,
     },
     (accessToken, refreshToken, profile, done) => {
       //console.log("access token:", accessToken);
